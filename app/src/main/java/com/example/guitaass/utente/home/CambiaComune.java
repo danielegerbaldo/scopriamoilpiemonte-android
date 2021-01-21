f04f2d535b0e33a26801a98314c7335be4264e71
flaaroman <flavio.roman@hotmail.it>
2021-01-21 23:06:49 +0100

    Aggiunto delle classi di Comune, activity e utilizzo di URL per database

---
+++ app/src/main/java/com/example/guitaass/utente/home/CambiaComune.java
@@ -0,0 +1,129 @@
+package com.example.guitaass.utente.home;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.gson.Gson;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.guitaass.DOM.Utente;
+import com.example.guitaass.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.google.gson.internal.bind.TypeAdapters.URL;
+
+
+public class CambiaComune extends AppCompatActivity implements OnItemSelectedListener {
+
+    private String strMessage;
+    private Spinner spinner;
+    private String result;
+    //JSONObject myData=null;
+    TextView textMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_common);
+        TextView textVecchioComune = findViewById(R.id.textCambiaComune);
+        textMessage = findViewById(R.id.textView);
+        String extras = getIntent().getExtras().getString("nome");
+        System.out.println(extras);
+        strMessage = "http://10.0.2.2:8080/api/v1/utente/nome/"+extras;
+        new MyTask().execute();
+        /*
+        //prendi le informazioni di utente
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null){
+            String data = extras.getString("utente");
+            System.out.println("Ora stampo l'utente --> " +data);
+            try {
+                myData = new JSONObject(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        */
+        //SPINNER
+        spinner = (Spinner) findViewById(R.id.spinner3);
+        /*
+        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.comuni, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        */
+        //BUTTON CHANGE CITY
+        Button changeComune = findViewById(R.id.changeComune);
+        changeComune.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newComune = spinner.getSelectedItem().toString();
+            }
+        });
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL url;
+            try {
+                url = new URL(strMessage);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+                String stringBuffer;
+                String string = "";
+                while ((stringBuffer = bufferedReader.readLine()) != null){
+                    string = String.format("%s%s", string, stringBuffer);
+                }
+                bufferedReader.close();
+                result = string;
+            } catch (IOException e){
+                e.printStackTrace();
+                result = e.toString();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            textMessage.setText(result);
+            super.onPostExecute(aVoid);
+        }
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),item,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
